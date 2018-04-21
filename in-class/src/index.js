5ac4a6926f354696d618117d92eeedc9f0634106
Michael Risley <michaelrisley@d-ip-10-194-79-76.ouguest.nor.ou.edu>
2018-04-21 11:27:33 -0500

    doing class components

--- in-class/src/index.js
+++ in-class/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const habits = ["learn react", "learn js", "run", "read"]
+const habits = ["Learn React", "Learn JS", "Run", "Read"]
 
 const CurrentHabits = () => {
 	const liHabits = habits.map((mappedHabits) => <li>{mappedHabits}</li>)
@@ -16,15 +16,26 @@ const CurrentHabits = () => {
 	)
 }
 
-const App = (props) => {
-	return (
+class App extends Component {
+	constructor(props) {
+	super(props)
+	this.state = {}
+}
+
+	render() {
+		return (
 		<div>
-		<h1>Welcome {props.name} {props.name2} {props.lastname} to Pure Components!</h1>
+		<h1>Welcome {this.props.name} {this.props.name2} {this.props.lastname} to Pure Components!</h1>
 		<h2>You are rendering more than one html tag now!</h2> 
 		<CurrentHabits />
 		</div> 
-	)
+		)	
+	}
 }
+
+
+
+
 
 ReactDOM.render(<App name="Michael" name2="Andrew" lastname="Risley" />, document.getElementById('root'));
 registerServiceWorker();
