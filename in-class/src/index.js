27fd631ac596d3633e1732bbb179c72fe45afc91
Michael Risley <michaelrisley@d-ip-10-194-79-76.ouguest.nor.ou.edu>
2018-04-21 10:52:20 -0500

    adding component to parent component App

--- in-class/src/index.js
+++ in-class/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-const Habits = ["learn react", "learn js", "run", "read"]
+const habits = ["learn react", "learn js", "run", "read"]
+
+const CurrentHabits = () => {
+	const liHabits = habits.map((mappedHabits) => <li>{mappedHabits}</li>)
+	return (
+	<div>
+		<ol>
+			{liHabits}
+		</ol>
+	</div>
+	)
+}
 
 const App = (props) => {
 	return (
 		<div>
 		<h1>Welcome {props.name} {props.name2} {props.lastname} to Pure Components!</h1>
 		<h2>You are rendering more than one html tag now!</h2> 
+		<CurrentHabits />
 		</div> 
 	)
 }
