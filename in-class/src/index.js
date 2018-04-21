a7150464044ef61b13d0f71b11ea5d52f5376f43
Michael Risley <michaelrisley@d-ip-10-194-79-76.ouguest.nor.ou.edu>
2018-04-21 10:40:51 -0500

    basic app

--- in-class/src/index.js
+++ in-class/src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Habits = ["learn react", "learn js", "run", "read"]
+
+const App = (props) => {
+	return (
+		<div>
+		<h1>Welcome {props.name} {props.name2} {props.lastname} to Pure Components!</h1>
+		<h2>You are rendering more than one html tag now!</h2> 
+		</div> 
+	)
+}
+
+ReactDOM.render(<App name="Michael" name2="Andrew" lastname="Risley" />, document.getElementById('root'));
 registerServiceWorker();
