5291361635f966cfd8a7e6a84839410422ff31ef
Michael Risley <michaelrisley@d-ip-10-194-79-76.ouguest.nor.ou.edu>
2018-04-21 12:30:25 -0500

    murdered by classes

---
+++ README.md
@@ -0,0 +1,5 @@
+Create React App
+
+(a) Creating Pure Components
+(b) Nesting Pure Components
+(c) Creating Class Components
--- in-class/src/index.js
+++ in-class/src/index.js
@@ -5,8 +5,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 const habits = ["Learn React", "Learn JS", "Run", "Read"]
 
-const CurrentHabits = () => {
-	const liHabits = habits.map((mappedHabits) => <li>{mappedHabits}</li>)
+const CurrentHabits = (props) => {
+	const liHabits = props.habits.map((mappedHabits) => <li>{mappedHabits}</li>)
 	return (
 	<div>
 		<ol>
@@ -16,26 +16,81 @@ const CurrentHabits = () => {
 	)
 }
 
+class UserHabits extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			habitName: "",
+			habits: []
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({ habitName: event.target.value });
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		this.setState((prevState) => {
+			return{
+				habitName: "",
+				habits: [ ...prevState.habits, prevState.habitName]
+			}
+		})
+
+		console.log(this.state)
+	}
+
+	render() {
+			return (
+			<div>
+	      		<form onSubmit={this.handleSubmit}>
+		        	<label>
+		          		Habit Name:
+		          		<input type="text" value={this.state.habitName} onChange={this.handleChange} />
+		        	</label>
+	        		<input type="submit" value="Submit" />
+	      		</form>
+      		</div>
+    		)
+  		}
+	}
+
 class App extends Component {
 	constructor(props) {
 	super(props)
-	this.state = {}
+	this.state = {
+		name: "Michael" 
+	}
 }
 
 	render() {
 		return (
 		<div>
-		<h1>Welcome {this.props.name} {this.props.name2} {this.props.lastname} to Pure Components!</h1>
+		<h1>Welcome {this.state.name} {this.state.name2} {this.state.lastname} to Class Components!</h1>
 		<h2>You are rendering more than one html tag now!</h2> 
-		<CurrentHabits />
+		<UserHabits />
 		</div> 
 		)	
 	}
 }
 
+class BoilerPlateClass extends Component {
+		constructor(props) {
+			super(props)
+			this.state = {
+			}
+		}
+
+	render() {
+			return (
+			<div>
+			</div>
+			)
+		}
+	}
 
 
 
-
-ReactDOM.render(<App name="Michael" name2="Andrew" lastname="Risley" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
